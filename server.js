bf59c2c19f37d5c52cd1f57674343260459f53aa
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-11-25 15:56:25 +0530

    css

--- server.js
+++ server.js
@@ -133,10 +133,10 @@ app.post('/convert', async (req, res) => {
     }
 
     const browser = await puppeteer.launch({
-      executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+      executablePath: 'C:\Program Files\Google\Chrome\Application\chrome.exe',
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
+    
 
     const page = await browser.newPage();
 
