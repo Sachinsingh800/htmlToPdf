984f91451df70cfe8638a28e4a5b977b55353297
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-11-25 23:33:00 +0530

    css

--- server.js
+++ server.js
@@ -90,15 +90,13 @@ app.use(cors());
 let browser;
 
 (async () => {
-  try {
-    browser = await puppeteer.launch({
-      executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-      ignoreDefaultArgs: ['--disable-extensions'],
-      headless: 'new',
-    });
-  } catch (error) {
-    console.error('Error launching Puppeteer:', error);
-  }
+  const browser = await puppeteer.launch(); // launch a browser (chromium by default but you can chose another one)
+  const page = await browser.newPage(); // open a page in the browser
+  await page.goto("https://printable-version-of-my-wbe-page.com", {
+    waitUntil: "networkidle2",
+  }); // visit the printable version of your page
+  await page.pdf({ format: "a4", path: "./my_file.pdf" }); // generate the PDF 🎉
+  await browser.close(); // don't forget to close the browser. Otherwise, it may cause performances issues or the server may even crash..
 })();
 
 app.get('/', async (req, res) => {
